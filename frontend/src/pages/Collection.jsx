a4572d9f1e0f68ec84cde2663acd99b27b02698f
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-04-19 15:47:41 +0530

    Update Collection.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -221,7 +221,15 @@ const Collection = () => {
         {filterProducts.length > 0 ? (
           <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
             {filterProducts.map((item, index) => (
-              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} originalPrice={item.originalPrice} className='mb-28'/>
+              <ProductItem
+                  key={item._id}
+                  name={item.name}
+                  id={item._id}
+                  price={item.price}
+                  image={item.image}
+                  originalPrice={item.originalPrice}
+                  className="mb-28"
+                />
             ))}
           </div>
         ) : (
