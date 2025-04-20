ac89d9b3a2e0f3afa42e1e5af727f4566c41a889
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-04-21 01:53:50 +0530

    Update globalCouponRoutes.js

--- backend/routes/globalCouponRoutes.js
+++ backend/routes/globalCouponRoutes.js
@@ -4,7 +4,8 @@ import {
   createGlobalCoupon,
   listGlobalCoupons,
   deleteGlobalCoupon,
-  toggleGlobalCoupon
+  toggleGlobalCoupon,
+  checkGlobalCoupon
 } from '../controllers/globalCouponController.js';
 
 const globalCouponRouter = express.Router();
@@ -14,5 +15,6 @@ globalCouponRouter.post('/create', adminAuth, createGlobalCoupon);
 globalCouponRouter.get('/list', adminAuth, listGlobalCoupons);
 globalCouponRouter.post('/delete', adminAuth, deleteGlobalCoupon);
 globalCouponRouter.post('/toggle', adminAuth, toggleGlobalCoupon);
+globalCouponRouter.post('/check', adminAuth, checkGlobalCoupon);
 
 export default globalCouponRouter;
