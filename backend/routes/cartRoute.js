a385fc3c1545b1292290b3086ce9fb296c792407
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-04-22 16:35:04 +0530

    Update cartRoute.js

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addToCart, getUserCart, updateCart, clearDeletedCartItems, updateCartItemColor } from '../controllers/cartController.js'
+import { addToCart, getUserCart, updateCart, clearDeletedCartItems, updateCartItemColor, removeCartItem } from '../controllers/cartController.js'
 import authUser from '../middleware/auth.js'
 
 const cartRouter = express.Router()
@@ -9,5 +9,6 @@ cartRouter.post('/add',authUser, addToCart)
 cartRouter.post('/update',authUser, updateCart)
 cartRouter.post("/clear-deleted", authUser, clearDeletedCartItems);
 cartRouter.post("/update-color", authUser, updateCartItemColor);
+cartRouter.post("/remove-item", authUser, removeCartItem);
 
 export default cartRouter
