98f96050b88cfb80d6ab7589ea518ef35b9165e6
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-04-25 19:38:24 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { loginUser,registerUser,adminLogin, verifyOtp, getAllUsers, deleteUser} from '../controllers/userController.js';
+import { loginUser,registerUser,adminLogin, verifyOtp, getAllUsers, deleteUser, getUserProfile } from '../controllers/userController.js';
 import adminAuth from '../middleware/adminAuth.js';
+import authUser from '../middleware/auth.js';
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/admin',adminLogin)
 userRouter.post('/verify', verifyOtp);
-
+userRouter.post('/profile', authUser, getUserProfile)
 userRouter.get('/admin/users', adminAuth, getAllUsers);
 userRouter.delete('/admin/users/:userId', adminAuth, deleteUser);
 
