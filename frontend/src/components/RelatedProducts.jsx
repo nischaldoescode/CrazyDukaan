593b2501c19218b2cd93b6aa757b518dd9392076
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-05-02 20:53:48 +0530

    Update RelatedProducts.jsx

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
 
-const RelatedProducts = ({category,subCategory, originalPrice}) => {
+const RelatedProducts = ({category,subCategory}) => {
 
     const { products } = useContext(ShopContext);
     const [related,setRelated] = useState([]);
